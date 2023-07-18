ff56966e3a4af4fb9bc18d5f606654e18c0c6764
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-07-18 11:37:54 -0400

    view list of tags

---
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/tags/Tag.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card, CardBody } from "reactstrap";
+
+const Tag = ({ tag }) => {
+
+
+    return (
+        <Card>
+            <CardBody>
+                <p>{tag.name}</p>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default Tag;
