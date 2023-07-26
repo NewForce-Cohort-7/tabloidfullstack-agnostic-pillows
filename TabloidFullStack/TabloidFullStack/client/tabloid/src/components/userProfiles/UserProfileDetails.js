23443e3d423492cd58f16a6fcc2b4e437879f367
Nicholas Myers <nic338@gmail.com>
2023-07-26 10:59:06 -0400

    added functionality so only current user can change profile image

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/userProfiles/UserProfileDetails.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/userProfiles/UserProfileDetails.js
@@ -12,6 +12,8 @@ export const UserProfileDetails = () => {
 
     const loggedInUser = JSON.parse(localStorage.getItem("userProfile"));
     const isAdmin = loggedInUser?.userType?.id === 1;
+    const localTabloidUser = localStorage.getItem("userProfile");
+    const tabloidUserObject = JSON.parse(localTabloidUser);
 
     useEffect(() => {
         getUserProfileById(id)
@@ -22,32 +24,36 @@ export const UserProfileDetails = () => {
     }, [id]);
 
     return (
-        !showEdit ? 
-        <Container>
-            <Button onClick={() => navigate(-1)}>Back to User Profiles</Button>
-            <Row>
-                <Col sm="12" md={{ size: 6, offset: 3 }}>
-                    <Card className="m-auto">
-                    <CardImg top width="100%" src={userProfile.imageLocation || 'https://media.tenor.com/EYUlar2QIe4AAAAd/plink.gif'} alt="Profile pic" />
-                    <Button onClick={() => setShowEdit(true)}>Change Profile Avatar</Button>
-                        <CardBody>
-                            <CardTitle tag="h4">{userProfile.displayName}</CardTitle>
-                            <CardSubtitle tag="h6" className="mb-2">Email: {userProfile.email}</CardSubtitle>
-                            <CardSubtitle tag="h6" className="mb-2">Full Name: {userProfile.firstName} {userProfile.lastName}</CardSubtitle>
-                            <CardSubtitle tag="h6" className="mb-2">User Type: {userProfile.userType?.name}</CardSubtitle>
-                            <CardSubtitle tag ="h6" className="mb-2">Creation Date: {
-                                new Date(userProfile.createDateTime).toLocaleDateString('en-US')
-                            }</CardSubtitle>
-                            {isAdmin && (
-                            <CardSubtitle tag="h6" className="mb-2">Account Status: {userProfile.isActive ? "Active" : "Deactivated"}</CardSubtitle>
-                            )}
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-        :
-        <UserProfileEdit userProfileProp={userProfile} setUserProfile={setUserProfile} setShowEdit={setShowEdit} isAdmin={isAdmin}/>
+        !showEdit ?
+            <Container>
+                <Button onClick={() => navigate(-1)}>Back to User Profiles</Button>
+                <Row>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                        <Card className="m-auto">
+                            <CardImg top width="100%" src={userProfile.imageLocation || 'https://media.tenor.com/EYUlar2QIe4AAAAd/plink.gif'} alt="Profile pic" />
+                            {(tabloidUserObject.id === userProfile.id) ? <>
+                                <Button onClick={() => setShowEdit(true)}>Change Profile Avatar</Button>
+                            </>
+                                : <></>
+                            }
+                            <CardBody>
+                                <CardTitle tag="h4">{userProfile.displayName}</CardTitle>
+                                <CardSubtitle tag="h6" className="mb-2">Email: {userProfile.email}</CardSubtitle>
+                                <CardSubtitle tag="h6" className="mb-2">Full Name: {userProfile.firstName} {userProfile.lastName}</CardSubtitle>
+                                <CardSubtitle tag="h6" className="mb-2">User Type: {userProfile.userType?.name}</CardSubtitle>
+                                <CardSubtitle tag="h6" className="mb-2">Creation Date: {
+                                    new Date(userProfile.createDateTime).toLocaleDateString('en-US')
+                                }</CardSubtitle>
+                                {isAdmin && (
+                                    <CardSubtitle tag="h6" className="mb-2">Account Status: {userProfile.isActive ? "Active" : "Deactivated"}</CardSubtitle>
+                                )}
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            :
+            <UserProfileEdit userProfileProp={userProfile} setUserProfile={setUserProfile} setShowEdit={setShowEdit} isAdmin={isAdmin} />
     );
 }
 
